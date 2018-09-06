ee23dc8d19718511cf0d9a3222921da747d6924c
Ed Kelly <ed@newlinelabs.com>
2018-09-06 16:52:57 +0100

    Request validation

---
+++ server/components/request-validation.js
@@ -0,0 +1,22 @@
+const swaggerTools = require('swagger-tools');
+const path = require('path');
+const YAML = require('yamljs');
+const spec = swaggerTools.specs.v2;
+
+module.exports = (app, opts) => {
+  const swagger = YAML.load(path.resolve(opts.path));
+
+  spec.validate(swagger, (err) => {
+    if (err) {
+      console.log(err);
+
+      process.exit();
+    }
+  });
+
+  swaggerTools
+    .initializeMiddleware(swagger, (middleware) => {
+      app.middleware('routes:before', middleware.swaggerMetadata());
+      app.middleware('routes:before', middleware.swaggerValidator());
+    });
+};
